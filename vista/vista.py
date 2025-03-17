91f38b71c579b995fca12459ec85f5d9d779b736
RakadSK <132318791+RakadSK@users.noreply.github.com>
2025-03-17 13:28:10 -0500

    HOTFIX
    Error con la logica de dos botones

--- vista/vista.py
+++ vista/vista.py
@@ -7,13 +7,82 @@ from PyQt5.QtWidgets import (QPushButton,
                              QWidget,
                              QDialog,
                              QLineEdit,
+                             QSpinBox,
+                             QRadioButton,
+                             QHBoxLayout,
+
                              )
 from PyQt5 import QtCore, Qt
 
-
 # Main GUI
 # Como nota pues le organice lo que ustedes tenian ya que estaba desactualizado
-# Rakad
+
+
+class VentanaMain (QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Menu Waze")
+        self.setGeometry(200, 200, 800, 800)
+        self.w1 = dict()
+        self.w2 = dict()
+        self.w3 = dict()
+
+        self.w1['b_bsc_usuario'] = QPushButton("Buscar Usuario")
+        self.w1['b_vr_usuarios'] = QPushButton("Ver Usuarios")
+        self.w1['b_vr_ubicaciones'] = QPushButton("Ver Ubicaciones")
+        self.w1['b_vr_configuracion'] = QPushButton(
+            "Ver Configuraciones de usuarios")
+        self.w1['b_ob_configuracion'] = QPushButton(
+            "Ver configuraciones de un usuario")
+
+        self.w2['b_vr_destino'] = QPushButton("Ver Destinos")
+        self.w2['b_vr_vehiculos'] = QPushButton("Ver Vehiculos")
+        self.w2['b_buscar_vehiculo'] = QPushButton("Buscar vehiculo")
+        self.w2['b_ob_tipovehi'] = QPushButton("Obtener tipo vehiculo")
+        self.w2['b_ob_destino'] = QPushButton("Obtener Destino más visitado")
+
+        self.w3['tbl_1'] = QTableWidget()
+        self.w3['tbl_2'] = QTableWidget()
+
+        self.cajamain = QHBoxLayout()
+        self.cajamain.setContentsMargins(0, 0, 0, 0)
+        self.cajamain.setSpacing(0)
+        self.cajamain.setAlignment(Qt.Qt.AlignTop)
+
+        self.cajad = QVBoxLayout()
+        self.cajad.setContentsMargins(0, 0, 0, 0)
+        self.cajad.setSpacing(0)
+        self.cajad.setAlignment(Qt.Qt.AlignRight)
+
+        self.layout_main = QVBoxLayout()
+        for key_w, w in self.w2.items():
+            self.cajad.addWidget(w)
+
+        self.cajai = QVBoxLayout()
+        self.cajai.setContentsMargins(0, 0, 0, 0)
+        self.cajai.setSpacing(0)
+        self.cajai.setAlignment(Qt.Qt.AlignLeft)
+
+        self.layout_main = QVBoxLayout()
+        for key_w, w in self.w1.items():
+            self.cajai.addWidget(w)
+
+        self.cajaa = QVBoxLayout()
+        self.cajaa.setContentsMargins(0, 0, 0, 0)
+        self.cajaa.setSpacing(0)
+        self.cajaa.setAlignment(Qt.Qt.AlignBottom)
+
+        self.layout_main = QVBoxLayout()
+        for key_w, w in self.w3.items():
+            self.cajaa.addWidget(w)
+
+        self.cajamain.addLayout(self.cajai)
+        self.cajamain.addLayout(self.cajad)
+        self.layout_main.addLayout(self.cajamain)
+        self.layout_main.addLayout(self.cajaa)
+        self.setLayout(self.layout_main)
+
+# Clase de boton bsc usuario
 
 
 class VentanaBuscarUsuario:
