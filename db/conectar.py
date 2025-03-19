b8615d90a505b5c678c51116d0b2e4b193637e28
Sebastián Duque Sepúlveda <132318791+RakadSK@users.noreply.github.com>
2025-03-19 00:59:29 -0500

    Ruta en windows
    Estaba en formato de mac

--- db/conectar.py
+++ db/conectar.py
@@ -2,13 +2,13 @@ import sqlite3
 from sqlite3 import Error
 
 
-ubicacionBD = "./db/database.db"
+ubicacionBD = """.\db\database.db"""
 
 
-def crear_conexion(rutaBD=ubicacionBD):
+def crear_conexion():
     objeto_conexion = None
     try:
-        objeto_conexion = sqlite3.connect(rutaBD)
+        objeto_conexion = sqlite3.connect(ubicacionBD)
     except Error as e:
         print("||||||||||||||||||||||||||||")
         print("Error de conexión con la BD")
