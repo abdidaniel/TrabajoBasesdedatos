151202512619b0312ec00db3b9915a55a3aec431
Sebastián Duque Sepúlveda <132318791+RakadSK@users.noreply.github.com>
2025-03-19 19:52:21 -0500

    HOTFIX VO Vehiculo

--- modelo/VO/VehiculoVO.py
+++ modelo/VO/VehiculoVO.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class VehiculoVO:
     id_vehiculo: Optional[int] = None
-    tipo_vehiculo: Optional[str] = None  
-    tipo_combustible: Optional[str] = None 
-    matricula: Optional[str] = None 
-    fk_usuario: Optional[int] = None  
+    tipo_vehiculo: Optional[str] = None
+    tipo_combustible: Optional[str] = None
+    matricula: Optional[str] = None
+    fk_usuario: Optional[int] = None
