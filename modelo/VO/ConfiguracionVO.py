842aefff6af54c98fc86e7a4f0e59e3506e0049c
Sebastián Duque Sepúlveda <132318791+RakadSK@users.noreply.github.com>
2025-03-19 19:51:21 -0500

    HOTFIX VO Configuracion

--- modelo/VO/ConfiguracionVO.py
+++ modelo/VO/ConfiguracionVO.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class ConfiguracionVO:
     id_confi: Optional[int] = None  # PK INT
-    fk_usuario: Optional[int] = None  # FK
-    fk_destino: Optional[int] = None  # FK
-    fk_visualizacion: Optional[int] = None  # FK
-    fk_vehiculo: Optional[int] = None  # FK
+    id_usuario: Optional[int] = None  # FK
+    id_destino: Optional[int] = None  # FK
+    id_visualizacion: Optional[int] = None  # FK
+    id_vehiculo: Optional[int] = None  # FK
     colpass: Optional[bool] = None  # BOOL
-    navegacion_check: Optional[str] = None  # ('Evita Peajes', 'No Evita')
+    alertas_trafico: Optional[bool] = None
+    idioma: Optional[str] = None  # BOOL
+    navegacion: Optional[str] = None  # ('Evita Peajes', 'No Evita')
